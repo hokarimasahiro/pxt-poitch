479ceb799d220bd0ba6a22bc1d408be7456c12b2
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2021-03-17 10:57:57 +0900

    更新 test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,18 @@
-// テストはここに来ます。このパッケージが拡張機能として使用されるときにはコンパイルされません。
+if (input.buttonIsPressed(Button.A)){
+    poitch.initPoitch(poitch.poitchType.poitch2)
+}else{
+    poitch.initPoitch(poitch.poitchType.poitch4)
+}
+basic.forever(function () {
+    if (poitch.testSwitch(poitch.poitchSW.p8)) {
+        basic.showString("8")
+    } else if (poitch.testSwitch(poitch.poitchSW.p12)) {
+        basic.showString("C")
+    } else if (poitch.testSwitch(poitch.poitchSW.p13)) {
+        basic.showString("D")
+    } else if (poitch.testSwitch(poitch.poitchSW.p16)) {
+        basic.showString("G")
+    } else {
+        basic.clearScreen()
+    }
+})
